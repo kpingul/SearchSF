a02b98833177eb1862545e88427ca5f9f8b91d30
kpingul <kirckpingul@gmail.com>
2015-08-13 00:07:33 -0700

    add new folder filters and initialization

---
+++ src/js/app/SingleVenue/filters/MenuFilter.js
@@ -0,0 +1,41 @@
+(function(){
+
+	'use strict';
+
+	angular.module('myApp')
+
+		.filter('menufilter', menufilter);
+
+		function menufilter(){
+
+			return function(value){
+	
+				var menu = []
+	
+				if(value){
+	
+					menu  = value.filter( function( item, index) {
+
+							if(item.name === "Menus"){
+						
+								return item;
+							}
+							if(item.name === "Drinks"){
+						
+								return item;
+							}
+					});
+			
+					return menu;
+				}
+			
+
+			};
+
+
+		}
+
+	
+
+
+}());
---
+++ src/js/app/initialization/app.run.js
@@ -0,0 +1,19 @@
+(function(){
+	
+	'use strict';
+
+	angular.module('myApp')
+
+		.run(['$rootScope', function($rootScope) {
+
+			$rootScope.$on('$stateChangeSuccess',function(){
+				console.log('ss')
+				  //quick fix when the broswer initially reloads, it gets
+				  //automatically positioned at the top
+					$("html, body").animate({ scrollTop: 0 }, 0);
+			})
+
+
+		}]);
+
+}());
