12dd7c9d5c2cdc70df73a649221a55eebda8e041
kpingul <kirckpingul@gmail.com>
2015-08-22 09:33:10 -0700

    add new feature that allows user to view popover (bootstrap) of hours using a directive called business hours tool bar

--- src/js/app/SingleVenue/controllers/SingleVenueCtrl.js
+++ src/js/app/SingleVenue/controllers/SingleVenueCtrl.js
@@ -6,21 +6,20 @@
 
 		.controller('SingleVenueCtrl', SingleVenueCtrl);
 
-		SingleVenueCtrl.$inject = ['$scope','MapService','slickBreakpoints', 'SingleVenueData', 'SimilarVenueData', '$state'];
+		SingleVenueCtrl.$inject = ['$scope','MapService','slickBreakpoints', 'SingleVenueData'];
 
-		function SingleVenueCtrl($scope,MapService, slickBreakpoints, SingleVenueData, SimilarVenueData, $state){
+		function SingleVenueCtrl($scope,MapService, slickBreakpoints, SingleVenueData){
 
-			var vm 		 	 = this;
-			vm.venue  	 	 = SingleVenueData;
-			vm.reviewLim 	 = 5;
-			vm.slickBP 	 	 = slickBreakpoints.breakpoints;
-			vm.similarVenues = SimilarVenueData;
-			console.log(vm.similarVenues)
+			var vm 		 = this;
+			vm.venue  	 = SingleVenueData;
+			vm.reviewLim = 5;
+			vm.slickBP 	 = slickBreakpoints.breakpoints;
+
 			MapService.setVenueMap(SingleVenueData.location.lat, SingleVenueData.location.lng, SingleVenueData.name);
 
-			vm.viewSimilarVenue = function(venueID){
-				$state.go('venue', {venueID: venueID})
-			}
+
+
+
 		}		
 
 
