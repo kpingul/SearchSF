2d31645ef6e5e4719cf421500667c062dfdfab4c
kpingul <kirckpingul@gmail.com>
2015-10-27 15:48:50 -0700

    fixed: process.env.PORT misspelling

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express'),
 	app     = express(),
-	port    = process.env.port || 3000;
+	port    = process.env.PORT || 3000;
 
-console.log(process.env);
 app.use(express.static(__dirname + '/'));
 
 app.listen(port, function() {
