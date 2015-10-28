7d1cc84122a79f9008f179cb5055b2c7e8430359
kpingul <kirckpingul@gmail.com>
2015-10-28 14:05:33 -0700

    updated: added section query to endpoint to make sure user only searches for food spots

--- src/js/app/Home/controllers/HomeCtrl.js
+++ src/js/app/Home/controllers/HomeCtrl.js
@@ -6,19 +6,29 @@
 
 		.controller('HomeCtrl', HomeCtrl);
 
-		HomeCtrl.$inject = ['$scope', '$timeout', '$state'];
+		HomeCtrl.$inject = ['$scope', '$timeout', '$state', 'FSDataService'];
 
-		function HomeCtrl($scope, $timeout, $state){
+		function HomeCtrl($scope, $timeout, $state, FSDataService){
 			
 			var vm = this;
-			vm.validResponse = false;
+			vm.validResponse = true;
 
 			vm.searchRequest = function(query) {
-				vm.validResponse = true;
-				$timeout(function() {
-					vm.validResponse = false;
-					$state.transitionTo('main', {type: query});
-				}, 2000);
+				vm.validResponse = false;
+				FSDataService
+					.getVenueLocationsByType(query)
+					.then(function( response ) {
+						console.log(response);
+						if( response.length > 0 ) {
+							vm.validResponse = true;
+							$state.transitionTo('main', {type: query});
+						} else {
+							//throw error message
+							vm.validResponse = true;
+							console.log('error');	
+						}
+					});
+				
 
 			}
 
