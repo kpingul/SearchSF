92f899bc237f2f5bafa8e0b30b2888eba263b3ef
kpingul <kirckpingul@gmail.com>
2015-11-01 12:01:25 -0800

    updated: added homectrl and searchvenue ctrl unit tests

--- public/tests/HomeCtrl.spec.js
+++ public/tests/HomeCtrl.spec.js
@@ -1,64 +1,64 @@
 describe('Testing', function() {
-	
-	var $scope,
-		$controller,
-		$httpBackend,
-		HomeCtrl,
-		FSDataService,
-		MapService;
 
-	beforeEach(function() {
-		module('myApp');
-		var mockData = [1, 2, 3, 4];
+    var $scope,
+        $controller,
+        $httpBackend,
+        HomeCtrl,
+        FSDataService,
+        MapService;
 
-		var mockService = {
-			getVenueLocationsByType: function(query) {
-				var defer = $q.defer();
+    beforeEach(function() {
+        module('myApp');
+        var mockData = [1, 2, 3, 4];
 
-			      defer.resolve([1, 2, 3, 4]);
+        var mockService = {
+            getVenueLocationsByType: function(query) {
+                var defer = $q.defer();
 
-			      return defer.promise;
-			}
-		}
+                defer.resolve([1, 2, 3, 4]);
 
-		inject(function(_$rootScope_, _$controller_, _FSDataService_, _$q_) {
+                return defer.promise;
+            }
+        }
 
-			$scope        = _$rootScope_.$new();
-			$rootScope    = _$rootScope_;
-            $controller   = _$controller_;
-			FSDataService =  _FSDataService_;
-			$q = _$q_;
-			 
+        inject(function(_$rootScope_, _$controller_, _FSDataService_, _$q_) {
 
-			HomeCtrl = $controller('HomeCtrl', {
-				$scope : $scope,
-				FSDataService: mockService
-			})
+            $scope = _$rootScope_.$new();
+            $rootScope = _$rootScope_;
+            $controller = _$controller_;
+            FSDataService = _FSDataService_;
+            $q = _$q_;
 
 
-		});
+            HomeCtrl = $controller('HomeCtrl', {
+                $scope: $scope,
+                FSDataService: mockService
+            })
 
-	});
 
-	it('should have a home ctrl', function() {
-		expect(HomeCtrl).toBeDefined();
-	});
-	it('should have FSDataService', function() {
-		expect(FSDataService).toBeDefined();
-	});
+        });
 
-	it('should have valid response variable', function() {
-		expect(HomeCtrl.validResponse).toBeDefined();
-		expect(HomeCtrl.validResponse).toBe(true);
-	});
+    });
 
-	it('should have search request method', function() {
-		expect(HomeCtrl.searchRequest).toBeDefined();
-	});
+    it('should have a home ctrl', function() {
+        expect(HomeCtrl).toBeDefined();
+    });
+    it('should have FSDataService', function() {
+        expect(FSDataService).toBeDefined();
+    });
 
-	it('should  when search request method is called', function() {
+    it('should have valid response variable', function() {
+        expect(HomeCtrl.validResponse).toBeDefined();
+        expect(HomeCtrl.validResponse).toBe(true);
+    });
 
-	});
+    it('should have search request method', function() {
+        expect(HomeCtrl.searchRequest).toBeDefined();
+    });
+
+    it('should  when search request method is called', function() {
+
+    });
 
 
 });
--- public/tests/SearchVenueCtrl.spec.js
+++ public/tests/SearchVenueCtrl.spec.js
@@ -1,5 +1,5 @@
 describe('Testing', function() {
-	
+
 	var $scope,
 		$rootScope,
 		$controller,
@@ -11,89 +11,87 @@ describe('Testing', function() {
 		mockService,
 		mockMapService;
 
-		beforeEach(function() {
-			module('myApp');
-				FSDataService = {
-					getVenueLocationsByType: function(query) {
-						var defer = $q.defer();
-						
-						defer.resolve({});
-					    
-					    return defer.promise;
-					}
-				}
+	beforeEach(function() {
+		module('myApp');
+		FSDataService = {
+			getVenueLocationsByType: function(query) {
+				var defer = $q.defer();
 
-				MapService = {
-					setSearchVenueMap: function(query) {
-					
-					},
-					getSearchVenueMap: function() {
+				defer.resolve({});
 
-					}
-				}
+				return defer.promise;
+			}
+		}
 
+		MapService = {
+			setSearchVenueMap: function(query) {
 
+			},
+			getSearchVenueMap: function() {
+
+			}
+		}
 
 
-			inject(function(_$rootScope_, _$controller_, _$q_) {
 
-				$scope        = _$rootScope_.$new();
-				$rootScope    = _$rootScope_;
-				$controller   = _$controller_;
-				$q = _$q_;
+		inject(function(_$rootScope_, _$controller_, _$q_) {
 
-			});
-
-			SearchVenueCtrl = $controller('SearchVenueCtrl', {
-				$scope : $scope,
-				FSDataService: FSDataService,
-				MapService: MapService
-			});
-
-			
-
+			$scope = _$rootScope_.$new();
+			$rootScope = _$rootScope_;
+			$controller = _$controller_;
+			$q = _$q_;
 
 		});
 
-		
-		it('should have a search venue ctrl', function() {
-			expect(SearchVenueCtrl).toBeDefined();
-		});		
-		it('should have FSDataService defined', function() {
-			expect(FSDataService).toBeDefined();
-		});		
-		it('should have MapService defined', function() {
-			expect(MapService).toBeDefined();
-		});		
-		it('should have declared ariables', function() {
-			expect(SearchVenueCtrl.venues).toBeDefined();
-			expect(SearchVenueCtrl.venueFilter).toBeDefined();
-			expect(SearchVenueCtrl.sortType).toBeDefined();
-			expect(SearchVenueCtrl.searchType).toBeDefined();
+		SearchVenueCtrl = $controller('SearchVenueCtrl', {
+			$scope: $scope,
+			FSDataService: FSDataService,
+			MapService: MapService
 		});
 
 
-		it('should have a duplicate method', function() {
-			expect(SearchVenueCtrl.duplicate).toBeDefined();
-		});
-		it('should return a string when duplicate method is called', function() {
-			expect(SearchVenueCtrl.duplicate("test", 1)).toEqual('test ');
-		});
-		it('should call FSDataService getVenueLocationsByType', function() {
 
-			$rootScope.$apply();
-			spyOn(FSDataService, 'getVenueLocationsByType').and.callThrough();
-			spyOn(MapService, 'getSearchVenueMap').and.callThrough();
-			spyOn(MapService, 'setSearchVenueMap').and.callThrough();
-			FSDataService.getVenueLocationsByType({})
-			MapService.getSearchVenueMap();
-			MapService.setSearchVenueMap();
-			expect(FSDataService.getVenueLocationsByType).toHaveBeenCalled();
-			expect(SearchVenueCtrl.venues).toEqual({});
-			expect(SearchVenueCtrl.venueFilter).toBeTruthy();
-			expect(SearchVenueCtrl.sortType).toEqual("");
-			expect(SearchVenueCtrl.searchType).toEqual("");
+	});
 
-		});
+
+	it('should have a search venue ctrl', function() {
+		expect(SearchVenueCtrl).toBeDefined();
+	});
+	it('should have FSDataService defined', function() {
+		expect(FSDataService).toBeDefined();
+	});
+	it('should have MapService defined', function() {
+		expect(MapService).toBeDefined();
+	});
+	it('should have declared ariables', function() {
+		expect(SearchVenueCtrl.venues).toBeDefined();
+		expect(SearchVenueCtrl.venueFilter).toBeDefined();
+		expect(SearchVenueCtrl.sortType).toBeDefined();
+		expect(SearchVenueCtrl.searchType).toBeDefined();
+	});
+
+
+	it('should have a duplicate method', function() {
+		expect(SearchVenueCtrl.duplicate).toBeDefined();
+	});
+	it('should return a string when duplicate method is called', function() {
+		expect(SearchVenueCtrl.duplicate("test", 1)).toEqual('test ');
+	});
+	it('should call FSDataService getVenueLocationsByType', function() {
+
+		$rootScope.$apply();
+		spyOn(FSDataService, 'getVenueLocationsByType').and.callThrough();
+		spyOn(MapService, 'getSearchVenueMap').and.callThrough();
+		spyOn(MapService, 'setSearchVenueMap').and.callThrough();
+		FSDataService.getVenueLocationsByType({})
+		MapService.getSearchVenueMap();
+		MapService.setSearchVenueMap();
+		expect(FSDataService.getVenueLocationsByType).toHaveBeenCalled();
+		expect(SearchVenueCtrl.venues).toEqual({});
+		expect(SearchVenueCtrl.venueFilter).toBeTruthy();
+		expect(SearchVenueCtrl.sortType).toEqual("");
+		expect(SearchVenueCtrl.searchType).toEqual("");
+
+	});
 
 });
